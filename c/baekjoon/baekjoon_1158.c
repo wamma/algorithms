a2870de24f6f0b643417338ee43325afd91a3ee7
Hyungjun <pparkheong96@naver.com>
2023-06-20 23:00:31 +0900

    6/020

--- c/baekjoon/baekjoon_1158.c
+++ c/baekjoon/baekjoon_1158.c
@@ -12,35 +12,36 @@ typedef struct
 	Node* tail;
 } CircularLinkedList;
 
-void push_back(CircularLinkedList* cll, int k)
+void push_back(CircularLinkedList* cll, int i)
 {
 	Node* new_node = (Node*)malloc(sizeof(Node));
-	new_node->value = k;
+	new_node->value = i;
 	new_node->next = NULL;
 
 	if (cll->tail == NULL) // 비어 있는경우
 	{
 		cll->tail = new_node;
 		new_node->next = new_node;
-	} else 
+	}
+	else
 	{
-		new_node->next = cll->tail->next;
-		cll->tail->next = new_node;
+		new_node->next = cll->tail->next; // new_node->next를 cll->tail->next로 설정함으로써 새로운 노드의 next 포인터를 첫 번째 노드를 가리키도록 합니다.
+		cll->tail->next = new_node; // tail의 next가 새로운 노드를 가리키게 한다.
 		cll->tail = new_node;
 	}
 }
 
-void erase(CircularLinkedList* cll, int k, int z)
+void erase(CircularLinkedList* cll, int k, int first) // first는 출력을 위한 변수
 {
 	Node* dest = cll->tail;
 	for (int i = 0; i < k; i++)
 	{
-		if (i == k - 1)
+		if (i == k - 1) // k번째 노드를 찾은 경우
 			cll->tail = dest;
 		dest = dest->next;
 	}
 
-	if (z == 0)
+	if (first == 0)
 		printf("%d", dest->value);
 	else
 		printf(", %d", dest->value);
@@ -57,8 +58,8 @@ int main()
 	CircularLinkedList cll;
 	cll.tail = NULL;
 
-	for (int i = 0; i < n; i++)
-		push_back(&cll, i + 1);
+	for (int i = 1; i <= n; i++)
+		push_back(&cll, i);
 
 	printf("<");
 	for (int i = 0; i < n; i++)
--- c/doubly_linked_list/test.c
+++ c/doubly_linked_list/test.c
@@ -8,15 +8,15 @@ typedef struct Song
 	char artist[100];
 	struct Song *prev;
 	struct Song *next;
-}	Song;
+} Song;
 
 typedef struct Playlist
 {
 	Song *head;
 	Song *tail;
-}	Playlist;
+} Playlist;
 
-void	init(Playlist *playlist)
+void init(Playlist *playlist)
 {
 	playlist->head = NULL;
 	playlist->tail = NULL;
@@ -32,7 +32,7 @@ void add_song(Playlist **playlists, const char *title, const char *artist)
 	else if (firstchar >= 'A' && firstchar <= 'Z')
 		index = firstchar - 'A';
 	else
-		return ;
+		return;
 	if (index >= 0 && index < 26)
 	{
 		Playlist *playlist = playlists[index];
@@ -54,23 +54,6 @@ void add_song(Playlist **playlists, const char *title, const char *artist)
 			new_song->prev = playlist->tail;
 			playlist->tail = new_song;
 		}
-	}
-}
-
-void remove_playlist(Playlist **playlists, char playlist_name)
-{
-	int index = -1;
-
-	if (playlist_name >= 'a' && playlist_name <= 'z')
-		index = playlist_name - 'a';
-	else if (playlist_name >= 'A' && playlist_name <= 'Z')
-		index = playlist_name - 'A';
-	else
-		return ;
-	if (index >= 0 && index < 26)
-	{
-		Playlist *playlist = playlists[index];
-		free_playlist(playlist);
 	}
 }
 
@@ -100,6 +83,54 @@ void free_playlist(Playlist *playlist)
 	playlist->tail = NULL;
 }
 
+void remove_song(Playlist **playlists, const char *title)
+{
+	char song_title = title[0];
+	int index = -1;
+
+	if (song_title >= 'a' && song_title <= 'z')
+		index = song_title - 'a';
+	else if (song_title >= 'A' && song_title <= 'Z')
+		index = song_title - 'A';
+	else
+		return;
+
+	if (index >= 0 && index < 26)
+	{
+		Playlist *playlist = playlists[index];
+		Song *curr = playlist->head;
+
+		while (curr != NULL)
+		{
+			if (strcmp(curr->title, title) == 0)
+			{
+				if (curr->prev == NULL) // 삭제할 노래가 첫번째 노래일 경우
+				{
+					playlist->head = curr->next;
+					if (playlist->head != NULL)
+						playlist->head->prev = NULL;
+				}
+				else if (curr->next == NULL) // 삭제할 노래가 마지막 노래일 경우
+				{
+					playlist->tail = curr->prev;
+					if (playlist->tail != NULL)
+						playlist->tail->next = NULL;
+				}
+				else // 삭제할 노래가 중간에 있을 경우
+				{
+					curr->prev->next = curr->next;
+					curr->next->prev = curr->prev;
+				}
+
+				free(curr);
+				return;
+			}
+
+			curr = curr->next;
+		}
+	}
+}
+
 int main()
 {
 	Playlist *playlists[26];
@@ -111,25 +142,49 @@ int main()
 
 	add_song(playlists, "Stay", "The Kid LAROI & Justin Bieber");
 	add_song(playlists, "Bad Habits", "Ed Sheeran");
-	add_song(playlists, "bbb", "i dont know");
+	add_song(playlists, "bbb", "Ed Sheeran");
 	add_song(playlists, "Industry Baby", "Lil Nas X & Jack Harlow");
 	add_song(playlists, "Fancy Like", "Walker Hayes");
 	add_song(playlists, "Kiss Me More", "Doja Cat Featuring SZA");
 	add_song(playlists, "Levitating", "Dua Lipa");
 	add_song(playlists, "Montero (Call Me By Your Name)", "Lil Nas X");
 
-	for(int i = 0; i < 26; i++)
+	// for (int i = 0; i < 26; i++)
+	// {
+	// 	printf("%c List:\n", 'a' + i);
+	// 	print_playlist(playlists[i]);
+	// 	printf("\n");
+	// }
+
+	printf("current playlist\n");
+	for (int i = 0; i < 26; i++)
 	{
-		printf("%c List:\n", 'a' + i);
-		print_playlist(playlists[i]);
-		printf("\n");
+		if (playlists[i]->head != NULL)
+		{
+			printf("%c List:\n", 'a' + i);
+			print_playlist(playlists[i]);
+			printf("\n");
+		}
 	}
 
-	for(int i = 0; i < 26; i++)
+	remove_song(playlists, "bbb");
+
+	printf("\n\n\nafter remove bbb\n");
+	for (int i = 0; i < 26; i++)
+	{
+		if (playlists[i]->head != NULL)
+		{
+			printf("%c List:\n", 'a' + i);
+			print_playlist(playlists[i]);
+			printf("\n");
+		}
+	}
+
+	for (int i = 0; i < 26; i++)
 	{
 		free_playlist(playlists[i]);
 		free(playlists[i]);
 	}
 
-	return (0);
+	return 0;
 }
