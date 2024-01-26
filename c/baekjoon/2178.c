3d86595c9e2d707a5a9927a43c2d0762355986f8
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-01-26 22:27:41 +0900

    백준 용액합성하기-14921 완료

--- c/baekjoon/2178.c
+++ c/baekjoon/2178.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
-#define MAX 100
+#define MAX 101
 
 typedef struct s_point
 {
 	int x;
 	int y;
-} t_point;
+}	t_point;
 
 t_point queue[MAX * MAX];
-int front = 0, rear = 0;
-
-int n, m;
-int map[MAX][MAX];
-int visit[MAX][MAX];
+int front = 0; rear = 0;
 
 void enqueue(int x, int y)
 {
@@ -22,51 +18,37 @@ void enqueue(int x, int y)
 	rear++;
 }
 
-t_point dequeue()
+t_point dequeue(void)
 {
 	return (queue[front++]);
 }
 
+int N, M;
+int map[MAX][MAX];
+int visited[MAX][MAX];
+
 int bfs(int start_x, int start_y, int dest_x, int dest_y)
 {
-	int move_x[4] = { 0, 0, 1, -1 }; // 상하좌우
-	int move_y[4] = { 1, -1, 0, 0 };
 	enqueue(start_x, start_y);
-	visit[start_x][start_y] = 1;
+	visited[start_x][start_y] = 1;
 
 	while (front < rear)
 	{
-		t_point current = dequeue();
-		int x = current.x;
-		int y = current.y;
-
-		if (x == dest_x && y == dest_y)
-			return (visit[x][y]);
-		for (int i = 0; i < 4; i++)
-		{
-			int next_x = x + move_x[i];
-			int next_y = y + move_y[i];
-
-			if (next_x >= 0 && next_x < n && next_y >= 0 && next_y < m && map[next_x][next_y] == 1 && visit[next_x][next_y] == 0)
-			{
-				enqueue(next_x, next_y);
-				visit[next_x][next_y] = visit[x][y] + 1;
-			}
-		}
+		
 	}
-	return (-1);
 }
 
 int main()
 {
+	scanf("%d %d", &N, &M);
 
-	scanf("%d %d", &n, &m);
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < M; j++)
 			scanf("%1d", &map[i][j]);
 	}
-	int result = bfs(0, 0, n - 1, m - 1);
+	int result = bfs(0, 0, N - 1, M - 1);
+	if (result == -1)
+		printf("Wrong Map\n");
 	printf("%d\n", result);
-	return (0);
 }
