075c7ffb00b19b7e96187b08dedd15d3df49b575
park hyung jun <55447614+wamma@users.noreply.github.com>
2025-04-30 11:22:24 +0900

    Time: 2 ms (12.83%), Space: 14.5 MB (11.46%) - LeetHub

--- LeetCode/Easy/1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ LeetCode/Easy/1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -4,15 +4,7 @@ public:
         int evenCount = 0;
 
         for (int num : nums) {
-            int digits = 0;
-            int tmp = num;
-
-            while (tmp > 0) {
-                tmp /= 10;
-                digits++;
-            }
-
-            if (digits % 2 == 0) {
+            if (to_string(num).length() % 2 == 0) {
                 evenCount++;
             }
         }
